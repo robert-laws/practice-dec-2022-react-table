9f99cb7397f98739a748da5da00f03b71d903b0d
Robert Laws <roblaws@me.com>
2022-12-13 11:11:23 +0300

    add header groups

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -6,24 +6,36 @@ export const Publications = () => {
   const columns = useMemo(
     () => [
       {
-        Header: 'Title',
-        Footer: 'Title',
-        accessor: 'title', // accessor is the "key" in the data
+        Header: 'Full Title',
+        Footer: 'Full Title',
+        columns: [
+          {
+            Header: 'Title',
+            Footer: 'Title',
+            accessor: 'title', // accessor is the "key" in the data
+          },
+          {
+            Header: 'Source Title',
+            Footer: 'Source Title',
+            accessor: 'sourceTitle', // accessor is the "key" in the data
+          },
+        ],
       },
       {
-        Header: 'Source Title',
-        Footer: 'Source Title',
-        accessor: 'sourceTitle', // accessor is the "key" in the data
-      },
-      {
-        Header: 'First Name',
-        Footer: 'First Name',
-        accessor: 'firstName', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Last Name',
-        Footer: 'Last Name',
-        accessor: 'lastName', // accessor is the "key" in the data
+        Header: 'AUTHOR',
+        Footer: 'AUTHOR',
+        columns: [
+          {
+            Header: 'First Name',
+            Footer: 'First Name',
+            accessor: 'firstName', // accessor is the "key" in the data
+          },
+          {
+            Header: 'Last Name',
+            Footer: 'Last Name',
+            accessor: 'lastName', // accessor is the "key" in the data
+          },
+        ],
       },
       {
         Header: 'Document Type',
