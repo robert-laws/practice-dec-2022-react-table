a0959fabbfa2c288f3e8f82afc1c46a5e6788a53
Robert Laws <roblaws@me.com>
2022-12-14 08:48:58 +0300

    add filtering timing delay

--- src/components/GlobalFilter.js
+++ src/components/GlobalFilter.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+import { useAsyncDebounce } from 'react-table';
 
 export const GlobalFilter = ({ filter, setFilter }) => {
+  const [value, setValue] = useState(filter);
+
+  const onChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 1000);
+
   return (
     <span>
       Search:{' '}
-      <input value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
+      <input
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      />
     </span>
   );
 };
